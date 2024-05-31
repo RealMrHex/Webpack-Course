1a117c4b4af7e7ec498cd36eaa0428440e901095
RealMrHex <RealMrHex@gmail.com>
2024-05-31 17:23:51 +0330

    add webpack dev server

--- src/main.js
+++ src/main.js
@@ -1,16 +1,3 @@
 import logger from "./logger.js";
 
 logger('Hello Internet!');
-logger('Hello World!');
-logger('Hello!');
-
-class Car {
-    startEngine()
-    {
-        console.log('engine started');
-    }
-}
-
-
-const ford = new Car();
-ford.startEngine();
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,10 +1,17 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './src/main.js',
-    output: {
-        filename: 'app.js',
-        path: path.resolve(__dirname, './dist')
+  entry: "./src/main.js",
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "./public"),
     },
-    mode: 'development'
+    compress: true,
+    port: 9000,
+  },
+  mode: "development",
 };
