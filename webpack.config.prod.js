90a043e1835a86fd9d01fb8cb7543a729ca0091e
RealMrHex <RealMrHex@gmail.com>
2024-05-31 19:06:30 +0330

    load font files

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -20,6 +20,10 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   mode: "production",
