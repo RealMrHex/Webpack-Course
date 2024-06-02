72e64d19ae2ee8d876510c3c2c8024cf59234505
RealMrHex <RealMrHex@gmail.com>
2024-06-02 20:11:14 +0330

    add babel loader

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -31,6 +31,18 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "ie 11" }]
+            ]
+          }
+        }
+      }
     ],
   },
   mode: "development",
