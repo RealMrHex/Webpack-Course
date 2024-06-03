4d40fc059ce2ab6f5ae5e62dd7a707bad2407f83
RealMrHex <RealMrHex@gmail.com>
2024-06-03 18:46:15 +0330

    enter mpa mode

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import Counter from "./components/counter/Counter";
-
-const counter = new Counter();
-counter.render();
-console.log('script executed!');
-console.log('script executed11223123123!');
